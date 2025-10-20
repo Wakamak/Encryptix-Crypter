adaa05a6a03971255532fab8b30131b515e9c3e8
Denis T. <deadspell21391@gmail.com>
2025-10-20 12:04:55 +0200

    AssemblyInfo.cs

---
+++ Encryptix Crypter/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Encryptix Crypter")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Encryptix by unknown")]
+[assembly: AssemblyProduct("Encryptix Crypter")]
+[assembly: AssemblyCopyright("Copyright © Encryptix 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("65636223-ED52-4C9F-88DD-F9235CD19036")]
+[assembly: AssemblyVersion("9.28.201.981")]
+[assembly: AssemblyFileVersion("9.28.201.981")]
